b846422546c45dd3f0aa84a8ad7cdec613b32fa2
YeeTiao1019 <n1533512@gmail.com>
2022-01-25 15:38:40 +0800

    加入專案檔案。

---
+++ Controllers/CampsController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository _campRepository;
+        private readonly IMapper _mapper;
+
+        public CampsController(ICampRepository campRepository, IMapper mapper)
+        {
+            this._campRepository = campRepository;
+            this._mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<ActionResult<CampModel[]>> GetCamps()
+        {
+            try
+            {
+                var Result = await _campRepository.GetAllCampsAsync();
+
+                return _mapper.Map<CampModel[]>(Result); ;
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+    }
+}
