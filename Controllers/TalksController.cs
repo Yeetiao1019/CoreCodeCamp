8f7d70ac7f3859a6efef12ca4e9c867cf43fcb72
YeeTiao1019 <n1533512@gmail.com>
2022-01-26 15:40:29 +0800

    新增與 CampsController 關聯的 Talks API，並完成使用 moniker 搜尋 Talks Action

---
+++ Controllers/TalksController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [ApiController]
+    [Route("api/camps/{moniker}/talks")]
+    public class TalksController : ControllerBase
+    {
+        private readonly ICampRepository _campRepository;
+        private readonly IMapper _mapper;
+        private readonly LinkGenerator _linkGenerator;
+
+        public TalksController(ICampRepository campRepository, IMapper mapper, LinkGenerator linkGenerator)
+        {
+            this._campRepository = campRepository;
+            this._mapper = mapper;
+            this._linkGenerator = linkGenerator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<TalkModel[]>> Get(string moniker)
+        {
+            try
+            {
+                var Talks = await _campRepository.GetTalksByMonikerAsync(moniker);
+
+                return _mapper.Map<TalkModel[]>(Talks);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,"伺服器錯誤");
+            }
+
+            return BadRequest();
+        }
+    }
+}
