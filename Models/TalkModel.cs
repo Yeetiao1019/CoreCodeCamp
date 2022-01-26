c21cba3f327da3d147a2298cac53d284e7ea925b
YeeTiao1019 <n1533512@gmail.com>
2022-01-26 16:41:57 +0800

    新增 Talk Model 的 Validation

--- Models/TalkModel.cs
+++ Models/TalkModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,10 @@ namespace CoreCodeCamp.Models
 {
     public class TalkModel
     {
+        [Required]
         public string Title { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Abstract { get; set; }
         public int Level { get; set; }
         public SpeakerModel Speaker { get; set; }
